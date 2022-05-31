31fe811598933fa7c3cfe6ddb5d9e12ca8dd51f0
Selvam <test.user.02042022@gmail.com>
2022-05-31 17:45:02 +0200

    Signup completed

--- apps/api/src/app/auth/auth.service.ts
+++ apps/api/src/app/auth/auth.service.ts
@@ -1,11 +1,16 @@
 import { Injectable } from '@nestjs/common';
 import { ConfigService } from '@nestjs/config';
 import { AppMetadata, AuthenticationClient, ManagementClient, ManagementClientOptions, UserMetadata } from 'auth0';
+import { Client } from "@microsoft/microsoft-graph-client";
+import { TokenCredentialAuthenticationProvider } from "@microsoft/microsoft-graph-client/authProviders/azureTokenCredentials"
+import { ClientSecretCredential } from "@azure/identity"
+import "isomorphic-fetch";
 
 @Injectable()
 export class AuthService {
   management: ManagementClient<AppMetadata, UserMetadata>;
   auth: AuthenticationClient;
+  msauth: Client;
   constructor(private config: ConfigService) {
     let managementOptions: ManagementClientOptions = {
       domain: this.config.get('AUTH0_DOMAIN'),
@@ -28,5 +33,17 @@ export class AuthService {
       clientId: this.config.get('AUTH0_CLIENT_ID'),
       clientSecret: this.config.get('AUTH0_CLIENT_SECRET'),
     });
+
+    //region Sign up in Azure B2C using Microsoft Graph API
+    let scopes: string[] = ["https://graph.microsoft.com/.default"];
+    const credential = new ClientSecretCredential(this.config.get('AZB2C_TENANT_ID'),
+      this.config.get('AZB2C_USER_SIGNUP_CLIENT_ID'),
+      this.config.get('AZB2C_USER_SIGNUP_SECRET'));
+      
+    const provider = new TokenCredentialAuthenticationProvider(credential, { scopes });
+    this.msauth = Client.initWithMiddleware ({
+      debugLogging: true,
+      authProvider: provider});
+    //endregion
   }
 }
